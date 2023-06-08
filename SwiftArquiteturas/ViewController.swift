91e6b801b27d10ceb91b908c288e2e504601294c
Hoff Silva <hoff.henry@gmail.com>
2023-06-08 15:31:15 -0400

    Initial Commit

---
+++ SwiftArquiteturas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftArquiteturas
+//
+//  Created by Hoff Silva on 2023-06-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
