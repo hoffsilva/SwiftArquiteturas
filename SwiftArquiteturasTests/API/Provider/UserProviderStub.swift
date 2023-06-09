ea7e78041d048d8d4eca598d61ec42cadc25c149
Hoff Silva <hoff.henry@gmail.com>
2023-06-08 22:35:25 -0400

    Added Unit Tests structure.

--- SwiftArquiteturasTests/API/Provider/UserProviderStub.swift
+++ SwiftArquiteturasTests/API/Provider/UserProviderStub.swift
@@ -1,14 +1,24 @@
 //
-//  UserProviderTestCase.swift
+//  UserProviderStub.swift
 //  SwiftArquiteturasTests
 //
 //  Created by Hoff Silva on 2023-06-08.
 //
 
 import XCTest
+import FirebaseAuth
+import FirebaseCore
+@testable import SwiftArquiteturas
 
-final class UserProviderTestCase: XCTestCase {
-
-    
+final class UserProviderFirebaseStub: UserProviderProtocol {
+	
+	
+	func register(parameters: [AnyHashable : Any], completionHandler: @escaping (Result<SwiftArquiteturas.UserModel, Error>) -> Void) {
+		<#code#>
+	}
+	
+	func login(parameters: [AnyHashable : Any], completionHandler: @escaping (Result<SwiftArquiteturas.UserModel, Error>) -> Void) {
+		<#code#>
+	}
 
 }
