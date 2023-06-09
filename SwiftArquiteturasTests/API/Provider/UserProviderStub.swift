6ecb2dfbeef76e0788f9178ee251f8d1106bfb22
Hoff Silva <hoff.henry@gmail.com>
2023-06-08 22:34:56 -0400

    Added Unit Tests target.

---
+++ SwiftArquiteturasTests/API/Provider/UserProviderStub.swift
@@ -0,0 +1,14 @@
+//
+//  UserProviderTestCase.swift
+//  SwiftArquiteturasTests
+//
+//  Created by Hoff Silva on 2023-06-08.
+//
+
+import XCTest
+
+final class UserProviderTestCase: XCTestCase {
+
+    
+
+}
